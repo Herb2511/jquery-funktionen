41a8009468b195c875960e99fa36b61865ea9c63
Herbert Haida <h.haida@digital-print-group.de>
2019-10-17 16:53:04 +0200

    Testing

--- calculate.js
+++ calculate.js
@@ -1,15 +1,33 @@
-$(function() {
-    $('.serving').bind('keyup', function(event) {
+// Calculate amount based on input serving
+$(function () {
+    $('.serving').bind('keyup', function (event) {
         var previousValue = parseFloat($("#previousServing").val());
         var newValue = parseFloat($(event.target).val());
         if (previousValue && newValue) {
-            $('.ingredient').each(function(index, elem) {
+            $('.ingredient').each(function (index, elem) {
                 var ingredientNow = $('.amount', elem);
                 var oldIngredientAmount = ingredientNow.text();
                 var newIngredientAmount = oldIngredientAmount * newValue / previousValue;
                 ingredientNow.text(newIngredientAmount);
             });
             $('#previousServing').val(newValue);
+
+            console.log("Die neue Menge beträgt:", newValue);
+
+            if (newValue <= 0) {
+                alert("Der Wert muss größer als 0 sein.")
+            };
         }
     });
 });
+
+// Hide and show ingredients
+$("#button").click(function () {
+    $(".ingredients").fadeOut();
+});
+
+$("#button2").click(function () {
+    $(".ingredients").fadeIn();
+});
+
+
